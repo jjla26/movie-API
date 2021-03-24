a509420485cb791c31113d225485f626cf525884
Julio Lopez <julioj.lopeza@gmail.com>
2021-03-24 13:02:35 +0100

    Added comments to the file

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ const app = express();
 app.use(express.static('public'));
 app.use(morgan('common'));
 
+// Route for the root url
 app.get('/', (req, res) => {
   res.send('Welcome to the Movie API');
 });
 
+// Route for movies pathname returns movies data
 app.get('/movies', (req, res) => {
   res.json(movies);
 });
 
+// Server listener
 app.listen(8080, () => {
   console.log('Server running on port 8080');
 });
