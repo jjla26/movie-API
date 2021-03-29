7a55b2865e30d632c55fba0908673519d2d5ee7c
Julio Lopez <julioj.lopeza@gmail.com>
2021-03-29 12:34:38 +0200

    Formatted movies route file

--- routes/movies.js
+++ routes/movies.js
@@ -5,15 +5,18 @@ const movies = require('../data/movies');
 
 // Route for movies pathname returns movies data
 router.get('/', (req, res) => {
-  return res.status(200)
-      .json({data: movies, total: movies.length,
-        message: 'Movies retrieved Succesfully'});
+  return res
+      .status(200)
+      .json({
+        data: movies,
+        total: movies.length,
+        message: 'Movies retrieved Succesfully',
+      });
 });
 
 // Route to get the movies by title
 router.get('/:title', (req, res) => {
-  return res.json(movies.find((movie) =>
-    movie.name === req.params.title));
+  return res.json(movies.find((movie) => movie.name === req.params.title));
 });
 
 // Route to get genre of a movie
