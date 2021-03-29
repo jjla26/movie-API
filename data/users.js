5958ba3d423e92f78f3f2c7e50d1f7e0b14bbe2a
Julio Lopez <julioj.lopeza@gmail.com>
2021-03-29 11:53:18 +0200

    Created users in memory data

---
+++ data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: 'Julio',
+    email: 'julioj@gmail.com',
+  },
+  {
+    name: 'Jonathan',
+    email: 'jonathan@gmail.com',
+  },
+  {
+    name: 'Kristina',
+    email: 'kristina@gmail.com',
+  },
+  {
+    name: 'Carlos',
+    email: 'carlos@gmail.com',
+  },
+];
+
+module.exports = users;
