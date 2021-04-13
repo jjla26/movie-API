6ee05f18126f36c06f1f6ea5145b77c98ceb1b29
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-13 09:38:34 +0200

    Added create and get routes for users with mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,49 @@
 const express = require('express');
 // eslint-disable-next-line
-const router = express.Router();
-const users = require('../data/users');
+const router = express.Router({mergeParams: true});
+// const users = require('../data/users');
+const Models = require('../models/users');
+
+const Users = Models.User;
+
+// Get a user by username
+router.get('/:username', (req, res) => {
+  Users.findOne({Username: req.params.username})
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json(error));
+});
 
 // Route to get all the users
 router.get('/', (req, res) => {
-  return res.status(200).json({
-    data: users,
-    total: users.length,
-    message: 'Users retrieved Succesfully',
-  });
+  Users.find()
+      .then( (users) => {
+        return res.status(200).json({
+          data: users,
+          total: users.length,
+          message: 'Users retrieved Succesfully',
+        });
+      })
+      .catch((error) => res.status(400).json(error));
 });
 
 // Route to create an user
 router.post('/', (req, res) => {
-  const newUser = req.body;
-  if (!newUser.name ) {
-    const message = 'User\'s name Missing';
-    return res.status(400).json({message: message});
-  } else if (!newUser.email ) {
-    const message = 'User\'s email Missing';
-    return res.status(400).json({message: message});
-  } else {
-    users.push(newUser);
-    return res.status(200).json({
-      data: newUser,
-      message: 'User added succesfully',
-    });
-  }
+  Users.findOne({Username: req.body.Username}).then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + ' already exists');
+    } else {
+      Users.create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      })
+          .then((user) => res.status(201).json(user))
+          .catch((error) => {
+            res.status(400).json(error);
+          });
+    }
+  });
 });
 
 // Route to edit an user
