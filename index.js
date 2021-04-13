6e49696e772d3805b5c69f01511fb64f3b6d72fd
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-13 09:37:11 +0200

    Added and connect mongoose to the project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
