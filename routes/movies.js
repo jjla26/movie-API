49be1f9342f0c2adbe7f70efd7c4779b5111b1f5
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-14 12:15:26 +0200

    Added authorization to movies endpoints

--- routes/movies.js
+++ routes/movies.js
@@ -3,11 +3,12 @@ const express = require('express');
 const router = express.Router();
 // const movies = require('../data/movies');
 const Models = require('../models/movies');
+const passport = require('passport');
 
 const Movies = Models.Movie;
 
 // Route for movies pathname returns movies data
-router.get('/', (req, res) => {
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
   Movies.find()
       .then((movies) => {
         return res.status(200).json({
@@ -20,57 +21,63 @@ router.get('/', (req, res) => {
 });
 
 // Route to get the movies by title
-router.get('/:Title', (req, res) => {
-  Movies.findOne({Title: req.params.Title})
-      .then((movie) => {
-        if (!movie) {
-          return res.status(400).json({
-            message: `${req.params.Title} was not found`,
-          });
-        } else {
-          return res.status(200).json({
-            data: movie,
-            message: 'Movie has been retrieved',
-          });
-        }
-      })
-      .catch((error) => res.status(400).json(error));
-});
+router.get('/:Title', passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+      Movies.findOne({Title: req.params.Title})
+          .then((movie) => {
+            if (!movie) {
+              return res.status(400).json({
+                message: `${req.params.Title} was not found`,
+              });
+            } else {
+              return res.status(200).json({
+                data: movie,
+                message: 'Movie has been retrieved',
+              });
+            }
+          })
+          .catch((error) => res.status(400).json(error));
+    },
+);
 
 // Route genre by name
-router.get('/:Name/genre', (req, res) => {
-  Movies.findOne({'Genre.Name': req.params.Name})
-      .then((movie) => {
-        if (!movie) {
-          return res.status(400).json({
-            message: `${req.params.Name} was not found`,
-          });
-        } else {
-          return res.status(200).json({
-            data: movie.Genre,
-            message: 'Genre has been retrieved',
-          });
-        }
-      })
-      .catch((error) => res.status(400).json(error));
-});
+router.get('/:Name/genre', passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+      Movies.findOne({'Genre.Name': req.params.Name})
+          .then((movie) => {
+            if (!movie) {
+              return res.status(400).json({
+                message: `${req.params.Name} was not found`,
+              });
+            } else {
+              return res.status(200).json({
+                data: movie.Genre,
+                message: 'Genre has been retrieved',
+              });
+            }
+          })
+          .catch((error) => res.status(400).json(error));
+    },
+);
 
 // Route director by name
-router.get('/:Name/director', (req, res) => {
-  Movies.findOne({'Director.Name': req.params.Name})
-      .then((movie) => {
-        if (!movie) {
-          return res.status(400).json({
-            message: `${req.params.Name} was not found`,
-          });
-        } else {
-          return res.status(200).json({
-            data: movie.Director,
-            message: 'Director has been retrieved',
-          });
-        }
-      })
-      .catch((error) => res.status(400).json(error));
-});
+router.get('/:Name/director', passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+      Movies.findOne({'Director.Name': req.params.Name})
+          .then((movie) => {
+            if (!movie) {
+              return res.status(400).json({
+                message: `${req.params.Name} was not found`,
+              });
+            } else {
+              return res.status(200).json({
+                data: movie.Director,
+                message: 'Director has been retrieved',
+              });
+            }
+          })
+          .catch((error) => res.status(400).json(error));
+    },
+);
 
 module.exports = router;
