90c271f42bad80019f16646432493fe4376323f7
Julio Lopez <julioj.lopeza@gmail.com>
2021-04-20 13:03:14 +0200

    Deactivated the authorization of all movies endpoint temporarily

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const Movies = Models.Movie;
 
 // Route for movies pathname returns movies data
-router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.get('/', (req, res) => {
   Movies.find()
       .then((movies) => {
         return res.status(200).json({
