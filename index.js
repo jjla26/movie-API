5b28df902616dc8fb09a57b9f52093dc9372da33
Julio Lopez <julioj.lopeza@gmail.com>
2021-05-05 00:15:35 +0200

    Added cors policy

--- index.js
+++ index.js
@@ -18,7 +18,20 @@ mongoose.connect(process.env.CONNECTION_URI, {
 });
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = ['http://localhost:1234', 'https://myflixapp-client.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // eslint-disable-next-line max-len
+      const message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+}));
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
